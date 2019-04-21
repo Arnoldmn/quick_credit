12e2af6e7cb8e3a6dc2918dcafd55bc7bafe8866
Mumbere Arnold <arnoldnzibuk@gmail.com>
2019-04-22 01:15:18 +0300

    changes made to apply loan

--- ui/js/index.js
+++ ui/js/index.js
@@ -70,3 +70,30 @@ function fixStepIndicator(n) {
     //... and adds the "active" class on the current step:
     x[n].className += " active";
 }
+
+function populateTable() {
+    // This function will populate the application table
+    var a = document.getElementById("idnum").value;
+    var b = document.getElementById("intrate").value;
+    var c = document.getElementById("duration").value;
+    var d = document.getElementById("reason").value;
+    var e = document.getElementById("amount").value;
+
+    document.getElementById("myidnum").innerHTML = a;
+    document.getElementById("myintreate").innerHTML = b;
+    document.getElementById("mypwork").innerHTML = c;
+    document.getElementById("mydurat").innerHTML = d;
+    document.getElementById("myreason").innerHTML = e;
+
+
+    var f2 = document.getElementById("idnum").value;
+    var g2 = document.getElementById("intrate").value;
+    var h2 = document.getElementById("mypwork").value;
+    var i2 = document.getElementById("mydurat").value;
+    var j2 = document.getElementById("myreason").value;
+
+
+
+
+
+}
